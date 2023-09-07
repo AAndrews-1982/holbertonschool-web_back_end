8d8b3740049514ed3cc173437c5772f7bd6b6cc8
AAndrews-1982 <altonandrews@icloud.com>
2023-09-07 21:29:14 +0000

    removed whitespace

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,10 +2,10 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  if (trueOrFalse) {
-      const newtask = false;
-      const newtask2 = true;
-      return [newtask, newtask2]  
+if (trueOrFalse) {
+  const newtask = false;
+  const newtask2 = true;
+  return [newtask, newtask2];
   }
 
   return [task, task2];
