22135bd423faba586e6b68bf03245f5672739731
AAndrews-1982 <altonandrews@icloud.com>
2023-09-08 01:23:08 +0000

    Attempting to fix constructor types error

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,6 +1,11 @@
-// 2-hbtn_course.js
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') throw TypeError('name must be a String');
+    if (typeof length !== 'number') throw TypeError('length must be a Number');
+    if (!Array.isArray(students)) throw TypeError('students must be an Array');
+    students.forEach((student) => {
+      if (typeof student !== 'string') throw TypeError('student must be a String');
+    });
     this._name = name;
     this._length = length;
     this._students = students;
