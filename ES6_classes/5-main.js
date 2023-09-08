e4c38d454368e8194e8a6a3028466cf26f826a9f
AAndrews-1982 <altonandrews@icloud.com>
2023-09-08 00:23:13 +0000

    Class Building

---
+++ ES6_classes/5-main.js
@@ -0,0 +1,14 @@
+// 5-main.js
+import Building from './5-building.js';
+
+const b = new Building(100);
+console.log(b);
+
+class TestBuilding extends Building {
+  evacuationWarningMessage() {
+    return 'This is a test evacuation warning message.';
+  }
+}
+
+const testBuilding = new TestBuilding(200);
+console.log(testBuilding.evacuationWarningMessage()); // Output: "This is a test evacuation warning message."
