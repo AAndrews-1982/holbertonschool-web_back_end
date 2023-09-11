12e0945b66bc5c395311c1e15c8833642e2854a3
AAndrews-1982 <altonandrews@icloud.com>
2023-09-11 23:11:35 +0000

    lint fix

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,5 @@
 function getStudentIdsSum(list) {
+  // eslint-disable-next-line prefer-arrow-callback, func-names
   const sumOfIds = list.reduce(function (accumulator, student) {
     return accumulator + student.id;
   }, 0);
