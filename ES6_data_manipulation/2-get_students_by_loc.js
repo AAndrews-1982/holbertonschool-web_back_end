7cc537a459103246a60552f229317b355bad5d21
AAndrews-1982 <altonandrews@icloud.com>
2023-09-11 22:25:29 +0000

    lintgit add .!

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,8 +1,5 @@
-const getStudentsByLocation = (studentList, city) =>
-  studentList.filter(student => student.location === city);
+function getStudentsByLocation(studentList, city) {
+  return studentList.filter(student => student.location === city);
+}
 
-// Example usage:
-const students = getListStudents(); // Assuming you have the previous getListStudents function defined
-const city = "San Francisco";
-const studentsInCity = getStudentsByLocation(students, city);
-console.log(studentsInCity);
+export default getStudentsByLocation;
