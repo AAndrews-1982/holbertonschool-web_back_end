ecfaecf51d0b0e9bb333e7cd8b716f6bf67a6693
AAndrews-1982 <altonandrews@icloud.com>
2023-09-12 14:07:53 +0000

    fixed createInt8TypedArray errrors

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,23 +1,13 @@
-function createInt8TypedArray(length, position, value) {
+export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
     throw new Error('Position outside range');
   }
 
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
-  view.setInt8(position, value);
+
+  const setInt8Value = (pos, val) => view.setInt8(pos, val);
+  setInt8Value(position, value);
 
   return view.buffer;
-}
-
-// Example usage:
-const length = 5;
-const position = 2;
-const value = 42;
-
-try {
-  const newArrayBuffer = createInt8TypedArray(length, position, value);
-  console.log(newArrayBuffer);
-} catch (error) {
-  console.error(error.message);
 }
