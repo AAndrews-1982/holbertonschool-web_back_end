6a66bd9665299fd967d4d24e4d91b341d9776966
AAndrews-1982 <altonandrews@icloud.com>
2023-09-13 00:33:27 +0000

    fixed lint error

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -6,7 +6,7 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
 
-if (value < -128 || value > 127 || !Number.isInteger(value)) {
+  if (value < -128 || value > 127 || !Number.isInteger(value)) {
     throw new Error('Value is not a valid Int8');
   }
 
