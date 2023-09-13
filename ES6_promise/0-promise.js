2a294200af5edec8fc198ce2648f8823399afca3
AAndrews-1982 <altonandrews@icloud.com>
2023-09-13 20:40:51 +0000

    same errors, trying again

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,7 +1,5 @@
 export default function getResponseFromAPI() {
-  return new Promise ((resolve) =>  {
+  return new Promise ((resolve) => {
   // Resolve the promise here with the desired data
-  const responseData = { message: 'Data from API'
-  resolve(responseData);
-{);
+  });
 }
