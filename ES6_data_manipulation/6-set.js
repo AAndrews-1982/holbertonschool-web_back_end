4e5ba57ea99b87ea91f63446d5e18a866e5abb13
AAndrews-1982 <altonandrews@icloud.com>
2023-09-13 00:36:34 +0000

    fixed array

--- ES6_data_manipulation/6-set.js
+++ ES6_data_manipulation/6-set.js
@@ -1,34 +1,3 @@
 export default function setFromArray(array) {
-  const uniqueArray = [...new Set(array)];
-
-  // Check if the array contains only booleans
-  const isBooleanArray = uniqueArray.every(item => typeof item === 'boolean');
-
-  // Check if the array contains only strings
-  const isStringArray = uniqueArray.every(item => typeof item === 'string');
-
-  // Check if the array contains only integers
-  const isIntArray = uniqueArray.every(item => Number.isInteger(item));
-
-  // Check if the array contains objects based on properties
-  const objToCheck = { id: 2, name: 'Jane' };
-  const isObjectInArray = uniqueArray.some(obj =>
-    obj.id === objToCheck.id && obj.name === objToCheck.name
-  );
-
-  return {
-    set: new Set(uniqueArray),
-    isBooleanArray,
-    isStringArray,
-    isIntArray,
-    isObjectInArray,
-  };
+  return new Set(array);
 }
-
-// Example usage:
-const result = setFromArray([12, 32, 15, 78, 98, 15]);
-console.log('Set:', [...result.set]);
-console.log('Contains only booleans:', result.isBooleanArray);
-console.log('Contains only strings:', result.isStringArray);
-console.log('Contains only integers:', result.isIntArray);
-console.log('Contains object:', result.isObjectInArray);
