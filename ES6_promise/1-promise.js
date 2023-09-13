71580941538ada61672cbeb6bc77431206b03be9
AAndrews-1982 <altonandrews@icloud.com>
2023-09-13 20:53:23 +0000

    fixed API and lint errors

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,11 +1,9 @@
 export function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success === true) {
-      const response = { status: 200, body: 'Success' };
-      resolve(response);
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
     } else {
-      const error = new Error('The fake API is not working currently');
-      reject(error);
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
