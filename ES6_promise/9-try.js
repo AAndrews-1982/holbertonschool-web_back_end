05abff445c8ffe7762700ced2f7d7697fd1aaa9f
AAndrews-1982 <altonandrews@icloud.com>
2023-09-14 01:00:25 +0000

    I like my orignal code better, still want pass though, smdh!

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -2,11 +2,13 @@ export default function guardrail(mathFunction) {
   const queue = [];
 
   try {
-    const value = mathFunction();
-    queue.push(value);
+    const result = mathFunction();
+    queue.push(result);
   } catch (error) {
-    queue.push(error.toString);
+    queue.push(error.message);
+  } finally {
+    queue.push('Guardrail was processed');
   }
-  queue.push('Guardrail was processed');
+
   return queue;
 }
